6c039a1285b8f5d0e567a89d3a65f04008fd1b42
Lukman Bello <lukman@paystack.com>
2019-07-25 21:26:07 +0100

    added plugin metrics tracker

--- payment/payments/paystack/Paystack.php
+++ payment/payments/paystack/Paystack.php
@@ -1,10 +1,48 @@
 <?php 
 
+class os_class_paystack_plugin_tracker {
+    var $public_key;
+    var $plugin_name;
+    function __construct($plugin, $pk){
+        //configure plugin name
+        //configure public key
+        $this->plugin_name = $plugin;
+        $this->public_key = $pk;
+    }
+
+   
+
+    function log_transaction_success($trx_ref){
+        //send reference to logger along with plugin name and public key
+        $url = "https://plugin-tracker.paystackintegrations.com/log/charge_success";
+
+        $fields = [
+            'plugin_name'  => $this->plugin_name,
+            'transaction_reference' => $trx_ref,
+            'public_key' => $this->public_key
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+        //execute post
+        $result = curl_exec($ch);
+        //  echo $result;
+    }
+}
+
 Class Paystack
 {
 
     public function __construct() { }
-
+ 
     public static function button($amount = '0.00', $description = '', $itemnumber = '101', $extra_array = null) {
         $extra = payment_prepare_custom($extra_array);
         $email = $extra_array['email'];
@@ -30,7 +68,7 @@ Class Paystack
                     data-key="<?php echo $public_key;?>"
                     data-email="<?php echo $email;?>"
                     data-amount="<?php echo $amount * 100;?>"
-                  
+                    data-metadata='{"custom_fields": [{"display_name": "Plugin","variable_name": "plugin","value": "os-class"}]}'
                   >
           </script>
         </form>
@@ -88,10 +126,12 @@ Class Paystack
         $reference = $_POST['paystack-trxref'];
 
         $key = osc_get_preference('paystack_tsk', 'payment');
-
+        $pk =  osc_get_preference('paystack_tpk', 'payment');
         if(osc_get_preference('paystack_sandbox', 'payment') == 0) {
             $key = osc_get_preference('paystack_lsk', 'payment');
+            $pk = osc_get_preference('paystack_lpk', 'payment');
         }
+        $pstk_logger = new os_class_paystack_plugin_tracker('os-class', $pk);
         $result = array();
         /// Verify Transaction was successful
         $url = 'https://api.paystack.co/transaction/verify/'.$reference;
@@ -110,6 +150,7 @@ Class Paystack
         }
 
         if (array_key_exists('data', $result) && array_key_exists('status', $result['data']) && ($result['data']['status'] === 'success')) {
+            $pstk_logger->log_transaction_success($reference);
             $status = 'success';
         }else{
             $status = 'failed';
